cb6f6d590fb8f569054ab883f56d8900f9509704
hujialin <512024290@qq.com>
2018-10-12 09:35:56 +0800

    1.cpp

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+int main()
+{	
+	
+	cout <<" pleases enter your first name:" ;
+	string name;
+	cin >> name;
+	cout << "hello ," << name <<"!!!!" << endl;
+	return 0;
+}
+	
